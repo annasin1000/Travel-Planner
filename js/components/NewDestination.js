ad584f1887f51fe651f991c89827626151ea70c0
annasin1000 <81631051+annasin1000@users.noreply.github.com>
2021-09-27 22:02:21 +0200

    destinations.js, NewDestination added

---
+++ js/components/NewDestination.js
@@ -0,0 +1,50 @@
+import React, {useState} from "react";
+//import button
+
+import {createDestination} from "../api/destinations";
+
+///onNewDestination????????????
+
+function NewDestination({onNewDestination}) {
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+
+    const handleAddDestination = e => {
+        e.preventDefault();
+        const destination = {
+            name,
+            description,
+            status: "open"
+        };
+
+        createDestination(destination, onNewDestination);
+    };
+    return (
+        <div className="">
+            <div className="">
+                <h1 className="">New destination</h1>
+                <form onSubmit={handleAddDestination}>
+                    <div className="">
+                        <input type=""
+                               className=""
+                               name=""
+                               placeholder=""
+                               value={name}
+                               onChange={e => setName(e.target.value)}/>
+                    </div>
+                    <div className="">
+                        <input type="text"
+                               className=""
+                               name="description"
+                               placeholder="Description"
+                               value={description}
+                               onChange={e => setDescription(e.target.value)}/>
+                    </div>
+                    <Button color={""} icon={""}>Add destination</Button>
+                </form>
+            </div>
+        </div>
+    );
+}
+export default NewDestination;
+
